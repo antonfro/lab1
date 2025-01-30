82fa17f0eb91152e02305ba0bd8ad84fbadfe2c1
antonfro <antonfrost0@gmail.com>
2025-01-30 16:09:41 +0100

    funkar?

---
+++ test/Saab95Test.java
@@ -0,0 +1,94 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class Saab95Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void getTowards() {
+    }
+
+    @Test
+    void getNrDoors() {
+    }
+
+    @Test
+    void getEnginePower() {
+    }
+
+    @Test
+    void getCurrentSpeed() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void setColor() {
+    }
+
+    @Test
+    void startEngine() {
+    }
+
+    @Test
+    void stopEngine() {
+    }
+
+    @Test
+    void decrementSpeed() {
+    }
+
+    @Test
+    void incrementSpeed() {
+    }
+
+    @Test
+    void speedFactor() {
+    }
+
+    @Test
+    void gas() {
+    }
+
+    @Test
+    void brake() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void turnLeft() {
+    }
+
+    @Test
+    void turnRight() {
+    }
+
+    @Test
+    void testSpeedFactor() {
+    }
+
+    @Test
+    void testIncrementSpeed() {
+    }
+
+    @Test
+    void testDecrementSpeed() {
+    }
+}
