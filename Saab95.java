3e56847ad54b526a6f6a5928ad6a36468edc2d25
antonfro <antonfrost0@gmail.com>
2025-01-30 16:04:20 +0100

    funkar?

---
+++ Saab95.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+
+public class Saab95 extends Car{
+    private boolean turboOn;
+
+    public Saab95(){
+        nrDoors = 2;
+        color = Color.red;
+        enginePower = 125;
+	    turboOn = false;
+        modelName = "Saab95";
+        stopEngine();
+    }
+
+    private void setTurboOn(){
+	    turboOn = true;
+    }
+
+    private void setTurboOff(){
+	    turboOn = false;
+    }
+
+    @Override
+    public double speedFactor(){
+        double turbo = 1;
+        if(turboOn) turbo = 1.3;
+        return enginePower * 0.01 * turbo;
+    }
+
+    @Override
+    public void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+    }
+
+    @Override
+    public void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+    }
+}
